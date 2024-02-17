90b96d9c25b0753af1b841ecce8562ad34c4ace8
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-02-17 23:40:37 +0700

    Update Data / Edit Data In Menu Users Is Worked

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,12 +43,23 @@ class UserController extends Controller
 
     // edit
     public function edit($id) {
-        return view('pages.dashboard');
+        $user = User::findOrFail($id);
+        return view('pages.user.edit', compact('user'));
     }
 
     // update
     public function update(Request $request, $id) {
-        return view('pages.dashboard');
+        $data = $request->all();
+        $user = User::findOrFail($id);
+        //check if password is not empty
+        if ($request->input('password')) {
+            $data['password'] = Hash::make($request->input('password'));
+        } else {
+            //if password is empty, then use the old password
+            $data['password'] = $user->password;
+        }
+        $user->update($data);
+        return redirect()->route('user.index');
     }
 
     // destroy
