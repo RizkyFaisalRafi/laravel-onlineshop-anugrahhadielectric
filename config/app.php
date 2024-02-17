6ba86a6698c182f0b647767d4602c526cde37e13
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-02-17 22:23:48 +0700

    Add Laravel Fortify for auth, login ui, and add folder field  components, layout from stisla template
    php artisan migrate
    in folder resources/views/components
    resources/views/layout

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
          * Package Service Providers...
          */
 
+        App\Providers\FortifyServiceProvider::class, // <-- Add Fortify
+
         /*
          * Application Service Providers...
          */
