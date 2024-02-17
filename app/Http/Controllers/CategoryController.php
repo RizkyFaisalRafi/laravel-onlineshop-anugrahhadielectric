4568149e2c9f1370a6de4f0ed2e6a5740bb91c05
Rizky Faisal Rafi <87520408+RizkyFaisalRafi@users.noreply.github.com>
2024-02-18 00:59:26 +0700

    Update Data / Edit Data In Menu Categories is Worked

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -42,12 +42,23 @@ class CategoryController extends Controller
 
     // edit
     public function edit($id) {
-        return view('pages.dashboard');
+        $user = Category::findOrFail($id);
+        return view('pages.category.edit', compact('user'));
     }
 
     // update
     public function update(Request $request, $id) {
-        return view('pages.dashboard');
+        $data = $request->all();
+        $user = Category::findOrFail($id);
+        //check if password is not empty
+        // if ($request->input('password')) {
+        //     $data['password'] = Hash::make($request->input('password'));
+        // } else {
+        //     //if password is empty, then use the old password
+        //     $data['password'] = $user->password;
+        // }
+        $user->update($data);
+        return redirect()->route('category.index');
     }
 
 
